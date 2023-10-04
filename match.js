57a96972ba3f9359042cf6bede052f26d6ba179e
susan stephens <crazeygrafix@gmail.com>
2023-10-03 20:39:24 -0600

    feat: got my basic html done and declared what i think will be my global variables

---
+++ match.js
@@ -0,0 +1,19 @@
+// Define an array of card values. Each card value appears twice (matching pairs).
+const cards = ['A', 'A', 'B', 'B', 'C', 'C'];
+
+// Initialize game-related variables.
+let score = 0;             // Player's score
+let flippedCards = [];     // Array to store the flipped (selected) cards
+let canFlip = false;       // Flag to control whether cards can be flipped
+let timer;                 // Timer variable to track the remaining time
+let timeLeft = 15;         // Initial time limit for the game
+let matchedPairs = 0;      // Counter for the number of matched card pairs
+const scoreboard = [];     // Array to store player scores
+
+// Get references to HTML elements using querySelector.
+const gameBoard = document.querySelector('.game-board');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time-left');
+const playerNameInput = document.querySelector('#player-name');
+const startButton = document.querySelector('#start-button');
+const submitScoreButton = document.querySelector('#submit-score');

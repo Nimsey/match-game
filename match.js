b4262c694be2436823bd9c8fcbe6e6064ce8c7fa
susan stephens <crazeygrafix@gmail.com>
2023-10-07 15:49:00 -0600

    feat: did more styling, adding sound

--- match.js
+++ match.js
@@ -11,6 +11,8 @@ let matchedPairs = 0;      // Counter for the number of matched card pairs
 const scoreboard = [];     // Array to store player scores
 // creating two decks of the same card (for future to maybe mix decks of different types)
 const shuffledCards = [...cards, ...cards];
+const clickSound = new Audio("https://cdn.discordapp.com/attachments/1156675538283409478/1160298468850532372/button-pressed-38129.mp3?ex=65342728&is=6521b228&hm=942862e3c99dda7b5080248719e79aa259d85d33e38ce6b5524645cde9322523&");
+
 
 // Get references to HTML elements using querySelector.
 const gameBoard = document.querySelector('.game-board');
@@ -20,6 +22,9 @@ const playerNameInput = document.querySelector('#player-name');
 const startButton = document.querySelector('#start-button');
 const submitScoreButton = document.querySelector('#submit-score');
 const gameOverText = document.querySelector(".game-over");
+const cardHolder = document.querySelector(".card-container");
+const loseText = document.createElement("div");
+const winText = document.createElement("div");
 //const cardContainer = document.querySelector('.card-container');
 
 gameOverText.style.display = "none";
@@ -41,7 +46,7 @@ function createCard(cardType) {
     card.textContent = cardType; // Set the card's content (A, B, C, etc.)
     // attach carc to the container
     cardContainer.append(card);
-    
+
     return cardContainer;
 }
 function generateCards() {
@@ -68,7 +73,18 @@ function flipCard(cardContainer) {
         setTimeout(checkMatch, 600);
     }
 }
-function checkMatch() {  
+// Add an event listener to each card container to handle the card flipping.
+gameBoard.addEventListener('click', (event) => {
+
+    const clickedCard = event.target.closest('.card-container');
+    if (clickedCard) {
+        clickSound.play();
+        flipCard(clickedCard);
+    }
+});
+
+function checkMatch() {
+    //clickSound.pause();
     // call the matched pair array
     const [card1, card2] = flippedCards;
     // if they match we get pts yay...
@@ -82,6 +98,7 @@ function checkMatch() {
         score -= 1;
         scoreDisplay.textContent = `Score: ${score}`;
         setTimeout(() => {
+            clickSound.pause();
             card1.classList.add("back");
             card2.classList.add("back");
             card1.classList.remove("flipped");
@@ -95,17 +112,14 @@ function checkMatch() {
     canFlip = true;
     // win logic: just checking the match arr against the double deck arr
     if (matchedPairs === shuffledCards.length / 2) {
-        gameBoard.textContent = "YOU WON";
+        // const winText = document.createElement("div");
+        winText.classList.add("winText");
+        winText.textContent = "YOU WON";
+        gameOverText.append(winText);
         endGame();
     }
 }
-// Add an event listener to each card container to handle the card flipping.
-gameBoard.addEventListener('click', (event) => {
-    const clickedCard = event.target.closest('.card-container');
-    if (clickedCard) {
-        flipCard(clickedCard);
-    }
-});
+
 // making a timer to make things challenging
 function startTimer() {
     timer = setInterval(() => {
@@ -114,16 +128,21 @@ function startTimer() {
         if (timeLeft <= 0) {
             clearInterval(timer);
             timeDisplay.textContent = 'Time: 0s';
-            
+
             if (matchedPairs !== shuffledCards.length / 2) {
-                gameBoard.textContent = "YOU LOST"; // clear the board
+
+                loseText.classList.add("loseText");
+                loseText.textContent = "YOU LOST!";
+                gameOverText.append(loseText);
+                endGame();
             }
-            endGame();
+
         }
     }, 1000); // Update the timer every second.
 }
 
-function startGame () {
+function startGame() {
+    gameBoard.textContent = " ";
     score = 0;
     scoreDisplay.textContent = 'Score: 0';
     timeLeft = 15;
@@ -134,23 +153,28 @@ function startGame () {
     canFlip = true;
     flippedCards = [];
     matchedPairs = 0; // Reset matchedPairs
-    gameBoard.textContent=" ";
+
     generateCards();
-    startTimer();
+    //startTimer();
 }
 // making a onClick function so the cards appear after its clicked
 function endGame() {
     canFlip = false;
+    //gameBoard.textContent = " ";
     // create the game over text and place it after the timer div
     gameOverText.style.display = "inline";
     // insert the form which the player can enter name
     const playerNameLabel = document.createElement('label');
-    playerNameLabel.textContent = 'Enter your name: ';
+    // playerNameLabel.textContent = 'Enter your name: ';
     playerNameInput.style.display = 'inline';
     submitScoreButton.style.display = 'inline';
-    gameBoard.appendChild(playerNameLabel);
-    gameBoard.appendChild(playerNameInput);
-    gameBoard.appendChild(submitScoreButton);
+
+    gameBoard.innerHTML = '';
+
+    gameBoard.prepend(submitScoreButton);
+    gameBoard.prepend(playerNameInput);
+    gameBoard.prepend(playerNameLabel);
+
 }
 
 
@@ -164,7 +188,7 @@ submitScoreButton.addEventListener('click', () => {
     // Create a new list item to display player's name and score
     const listItem = document.createElement('li');
     listItem.textContent = `${playerName}: ${score}`;
-    
+
     // Append the list item to the scoreboard
     const scoreList = document.querySelector('#score-list');
     scoreList.appendChild(listItem);
@@ -173,9 +197,10 @@ submitScoreButton.addEventListener('click', () => {
     playerNameInput.value = '';
     startButton.disabled = false;
     gameBoard.innerHTML = '';
+    gameOverText.style.display = "none";
 });
 
 // making a onClick function so the cards appear after its clicked
-startButton.addEventListener("click", function () {  
-    startGame();   
+startButton.addEventListener("click", function () {
+    startGame();
 });
